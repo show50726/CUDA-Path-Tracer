fe51018b109151a9969901e4825c1855918a982a
Doris Wu <doriswu@google.com>
2025-06-04 00:15:44 +0800

    Make functions inline

--- src/math.h
+++ src/math.h
@@ -7,15 +7,15 @@
 
 namespace math {
 
-__device__ float absDot(const glm::vec3& a, const glm::vec3& b) {
+__device__ inline float absDot(const glm::vec3& a, const glm::vec3& b) {
     return glm::abs(glm::dot(a, b)); 
 }
 
-__device__ float absCosTheta(glm::vec3 w) {
+__device__ inline float absCosTheta(glm::vec3 w) {
     return glm::abs(w.z); 
 }
 
-__device__ glm::vec2 sampleUniformDisk(glm::vec2 u) {
+__device__ inline glm::vec2 sampleUniformDisk(glm::vec2 u) {
     float theta = u.y * TWO_PI;
     return glm::vec2(glm::cos(theta), glm::sin(theta)) * glm::sqrt(u.x);
 }
@@ -25,7 +25,7 @@ __device__ inline glm::vec3 sampleHemisphereCosine(glm::vec2 u) {
     return glm::vec3(d, glm::sqrt(1.0f - glm::dot(d, d)));
 }
 
-__device__ float cosineHemispherePDF(float cosTheta) {
+__device__ inline float cosineHemispherePDF(float cosTheta) {
     return cosTheta * INV_PI;
 }
 
